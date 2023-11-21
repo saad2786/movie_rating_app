30b0f240f02f54084e43eec30dc07aa20110bbd8
saad2786 <patilsaad2786@gmail.com>
2023-11-21 15:43:43 +0530

    .env file added

--- src/App.js
+++ src/App.js
@@ -27,32 +27,32 @@ const tempMovieData = [
   },
 ]
 
-const tempWatchedData = [
-  {
-    imdbID: 'tt1375666',
-    title: 'Inception',
-    Year: '2010',
-    poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: 'tt0088763',
-    title: 'Back to the Future',
-    Year: '1985',
-    poster:
-      'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-]
+// const tempWatchedData = [
+//   {
+//     imdbID: 'tt1375666',
+//     title: 'Inception',
+//     Year: '2010',
+//     poster:
+//       'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+//     runtime: 148,
+//     imdbRating: 8.8,
+//     userRating: 10,
+//   },
+//   {
+//     imdbID: 'tt0088763',
+//     title: 'Back to the Future',
+//     Year: '1985',
+//     poster:
+//       'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+//     runtime: 116,
+//     imdbRating: 8.5,
+//     userRating: 9,
+//   },
+// ]
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
-const KEY = '17c91520'
+const KEY = process.env.REACT_APP_API_KEY
 
 export default function App() {
   // const [watched, setWatched] = useState()
